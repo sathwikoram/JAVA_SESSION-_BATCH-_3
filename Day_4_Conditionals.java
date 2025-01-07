f55d5a192f1b1d491f4f953ac81eacad1a48792c
sathwikoram <168582902+sathwikoram@users.noreply.github.com>
2025-01-07 21:19:12 +0530

    Add files via upload

---
+++ Day_4_Conditionals.java
@@ -0,0 +1,41 @@
+public class Day_4_Conditionals {
+    public static void main(String[] args){
+// fizzBuzz
+   /*public String fizzString(String str) {
+   if(str.charAt(0) == 'f' && str.charAt(str.length() -1)=='b'){
+    return "FizzBuzz";
+   }
+  else if(str.charAt(0) == 'f'){
+    return "Fizz";
+  }
+  else if(str.charAt(str.length()-1) == 'b'){
+    return "Buzz";
+  }
+  
+  return str;
+}
+
+ */
+
+ //squirrelPlay
+ /*public boolean squirrelPlay(int temp, boolean isSummer) {
+  if(isSummer == true){
+    if(temp>= 60 && temp<=100){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  else{
+    if(temp>=60 && temp<=90){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
+
+  */
+    }
+}
