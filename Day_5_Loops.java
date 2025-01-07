3b5730aece5f8fb3e4d7ba328fd0bb8056b8df33
sathwikoram <168582902+sathwikoram@users.noreply.github.com>
2025-01-07 21:19:31 +0530

    Add files via upload

---
+++ Day_5_Loops.java
@@ -0,0 +1,16 @@
+public class Day_5_Loops {
+    public static void main(String[] args) {
+        /*for(int i=1;i<=10;i++){
+            System.out.println("Sai");
+        }*/
+
+        /*String s="Phone";
+        for(int i=0;i<s.length();i++){
+            System.out.println(s.charAt(i));
+        }*/
+
+        //System.out.println("b"+1+"a"+2+8);
+
+        
+    }
+}
