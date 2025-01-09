f033f1a7d7bd2866fd7280847332ea193fdf1747
sathwikoram <168582902+sathwikoram@users.noreply.github.com>
2025-01-09 23:25:01 +0530

    Add files via upload

---
+++ Day_7.java
@@ -0,0 +1,28 @@
+public class Day_7 {
+    public static void main(String[] args){
+        //contains Duplicate
+        /*int[] arr={1,2,3,4,4};
+        for(int i=0;i<=arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println("True");
+                } 
+            }
+        }*/
+
+
+        //LeetCode Problem
+        /*class Solution {
+    public boolean containsDuplicate(int[] nums) {//{2,1,3,1}
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                return true;
+                }
+            }
+        }
+        return false;
+    }
+} */
+    }
+}
